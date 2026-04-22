8cca93b46d284b90638829770f2e445dbc19babc
Alex Ekums <alexekums@gmail.com>
2026-04-22 22:21:34 +0100

    Added Capacitor + Android platform + index.html for APK build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lexbank.app',
+  appName: 'LexBank',
+  webDir: 'dist'
+};
+
+export default config;
